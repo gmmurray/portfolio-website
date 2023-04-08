81ab0355add76cd7f1fd85be792a25ee20f723cc
Greg Murray <44830296+gmmurray@users.noreply.github.com>
2023-04-08 13:58:16 -0400

    google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import Script from 'next/script';
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,6 +11,23 @@ export default function Document() {
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
         <link rel="icon" href="/icon.png" />
+        <Script
+            strategy='afterInteractive'
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+          />
+          <Script
+          id="google-analytics"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       </Head>
       <body>
         <Main />
