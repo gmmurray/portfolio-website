08ce83677ddd88071043908506b201d7fdfff9b9
gmmurray <trinilime123@live.com>
2023-04-09 17:38:16 -0400

    analytics x2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,25 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import type { AppProps } from 'next/app';
 import { ContactProvider } from '../components/ContactProvider';
+import { Fragment } from 'react';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 import Head from 'next/head';
 import { getTheme } from '../config/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={getTheme()}>
-      <CssBaseline />
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <title>Greg Murray - Software Engineer</title>
-      </Head>
-      <ContactProvider>
-        <Component {...pageProps} />
-      </ContactProvider>
-    </ThemeProvider>
+    <Fragment>
+      <GoogleAnalytics trackPageViews />
+      <ThemeProvider theme={getTheme()}>
+        <CssBaseline />
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <title>Greg Murray - Software Engineer</title>
+        </Head>
+        <ContactProvider>
+          <Component {...pageProps} />
+        </ContactProvider>
+      </ThemeProvider>
+    </Fragment>
   );
 }
